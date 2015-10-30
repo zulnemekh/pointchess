21949244d2184891b02f51827506c198bf350adc
zulnemekh <bekhbat_zulnemekh@unimedia.co.jp>
2015-10-30 18:01:11 +0800

    some updated

---
+++ app/assets/javascripts/js/myjs/engine/fenDataDavuu.js
@@ -0,0 +1,44 @@
+// End game classic position
+mate2a=[
+[
+'[INFO "Temee iluu togosgol"]',
+'[FEN "1n3rk1/ppp2ppp/8/8/8/8/PPP2PPP/1NB2RK1 w - - 0 1"]',
+'[FES "1. Qxh6+ Rxh6 2. Rg8#  "]',
+'[TEST ""]'
+],[
+'[INFO "Mori iluu togosgol "]',
+'[FEN "3b1rk1/ppp2ppp/8/8/8/8/PPP2PPP/1NB2RK1 w - - 0 1"]',
+'[FES "1. Qg4+ Bxg4 2. Bf7# "]',
+'[TEST ""]'
+],[
+'[INFO "Tereg iluu togosgol"]',
+'[FEN "3b2k1/ppp2ppp/8/8/8/8/PPP2PPP/1N3RK1 w - - 0 1"]',
+'[FES "1. Qxh6+ Kxh6 2. Rh3# "]',
+'[TEST ""]'
+],[
+'[INFO "Khuu iluu hongon bodtoi togosgol"]',
+'[FEN "33b2k1/pp3ppp/8/8/8/8/PPPP1PPP/1N4K1 w - - 0 1"]',
+'[FES "1. Qg6+ hxg6 2. Bxg6# "]',
+'[TEST ""]'
+],[
+'[INFO "Khuu iluu togosgol"]',
+'[FEN "6k1/pp3ppp/8/8/8/8/PPP2PPP/6K1 w - - 0 1"]',
+'[FES "1... Rg1+ 2. Kxg1 Rxf1# "]',
+'[TEST ""]'
+],[
+'[INFO "2 khuu iluu togosgol"]',
+'[FEN "6k1/ppp3pp/8/8/8/8/PPPP1PPP/6K1 w - - 0 1"]',
+'[FES "1. Qxh5+ Rxh5 2. Bg6# "]',
+'[TEST ""]'
+],[
+'[INFO "Temee iluu togosgol"]',
+'[FEN "1n3rk1/ppp2ppp/8/8/8/8/PPP2PPP/1NB2RK1 w - - 0 1"]',
+'[FES "1. Qxh6+ Rxh6 2. Rg8#  "]',
+'[TEST ""]'
+],[
+'[INFO "Temee iluu togosgol"]',
+'[FEN "1n3rk1/ppp2ppp/8/8/8/8/PPP2PPP/1NB2RK1 w - - 0 1"]',
+'[FES "1. Qxh6+ Rxh6 2. Rg8#  "]',
+'[TEST ""]'
+]
+]
---
+++ app/assets/javascripts/js/myjs/engine/fenDataEndGame.js
@@ -0,0 +1,34 @@
+// End game classic position
+mate2a=[
+[
+'[INFO "Khuu noyonii togosgol"]',
+'[FEN "6k1/ppp2ppp/8/8/8/8/PPP2PPP/6K1 w - - 0 1"]',
+'[FES "1. Qxh6+ Rxh6 2. Rg8#  "]',
+'[TEST ""]'
+],[
+'[INFO "Khuu Morinii togosgol "]',
+'[FEN "1n4k1/ppp2ppp/8/8/8/8/PPP2PPP/1N4K1 w - - 0 1"]',
+'[FES "1. Qg4+ Bxg4 2. Bf7# "]',
+'[TEST ""]'
+],[
+'[INFO "Khuu mori temeenii togosgol"]',
+'[FEN "1n1b2k1/ppp2ppp/8/8/8/8/PPP2PPP/1NB3K1 w - - 0 1"]',
+'[FES "1. Qxh6+ Kxh6 2. Rh3# "]',
+'[TEST ""]'
+],[
+'[INFO "Khuu temeenii togosgol"]',
+'[FEN "3b2k1/ppp2ppp/8/8/8/8/PPP2PPP/2B3K1 w - - 0 1"]',
+'[FES "1. Qg6+ hxg6 2. Bxg6# "]',
+'[TEST ""]'
+],[
+'[INFO "Temee teregnii togosgol"]',
+'[FEN "3b1rk1/ppp2ppp/8/8/8/8/PPP2PPP/2B2RK1 w - - 0 1"]',
+'[FES "1... Rg1+ 2. Kxg1 Rxf1# "]',
+'[TEST ""]'
+],[
+'[INFO "Neg neg hongon bodtoi togosgol"]',
+'[FEN "1n1b1rk1/ppp2ppp/8/8/8/8/PPP2PPP/1NB2RK1 w - - 0 1"]',
+'[FES "1. Qxh5+ Rxh5 2. Bg6# "]',
+'[TEST ""]'
+]
+]
