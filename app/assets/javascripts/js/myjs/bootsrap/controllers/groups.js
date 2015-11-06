b0f45c735830a23cb9fdf2519304569342183393
zulnemekh <bekhbat_zulnemekh@unimedia.co.jp>
2015-11-06 18:05:30 +0800

    some update

---
+++ app/assets/javascripts/js/myjs/bootsrap/controllers/groups.js
@@ -0,0 +1,16 @@
+
+// app/scripts/controllers/groups.js
+ 
+'use strict';
+ 
+/**
+ * @ngdoc function
+ * @name fakeLunchHubApp.controller:GroupsCtrl
+ * @description
+ * # GroupsCtrl
+ * Controller of the fakeLunchHubApp
+ */
+angular.module('fakeLunchHubApp')
+  .controller('GroupsCtrl', ['$scope', 'Group', function ($scope, Group) {
+    $scope.groups = Group.query();
+  }]);
