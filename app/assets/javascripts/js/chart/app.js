1551181d5df6de6ebe0f8cb898b5e5d70b07cc3c
zulnemekh <bekhbat_zulnemekh@unimedia.co.jp>
2015-11-10 11:07:56 +0800

    angular js imported

--- app/assets/javascripts/js/chart/app.js
+++ app/assets/javascripts/js/chart/app.js
@@ -1,48 +1,20 @@
-'use strict';
+var phonecatApp = angular.module('phonecatApp', [
+  'ngRoute',
+  'phonecatControllers'
+]);
 
-var app = angular.module('demo-angular-chartjs', ['chartjs']);
-
-(function () {
-  'use strict';
-
-  app.controller('TestCtrl', function ($scope) {
-    $scope.lineChartData = {
-      labels: [
-        'Jan', 
-        'Feb', 
-        'Mar'
-      ],
-      datasets: [
-        {
-          data: [0, 5, 10, 15, 20, 25]
-        },
-        {
-          data: [3, 6, 9, 12, 15, 18]
-        }
-      ]
-    };
-    
-    $scope.lineChartData2 = {
-      labels: [
-        'Apr', 
-        'May', 
-        'Jun'
-      ],
-      datasets: [
-        {
-          data: [1, 7, 15, 19, 31, 40]
-        },
-        {
-          data: [6, 12, 18, 24, 30, 36]
-        }
-      ]
-    };
-    
-    $scope.activeData = $scope.lineChartData;
-
-    $scope.swapData = function () {
-      $scope.activeData = $scope.lineChartData2;
-    };
-  });
-
-})();
+phonecatApp.config(['$routeProvider',
+  function($routeProvider) {
+    $routeProvider.
+      when('/phones', {
+        templateUrl: '/assets/js/chart/partials/phone-list.html',
+        controller: 'PhoneListCtrl'
+      }).
+      when('/phones/:phoneId', {
+        templateUrl: '/assets/js/chart/partials/phone-detail.html',
+        controller: 'PhoneDetailCtrl'
+      }).
+      otherwise({
+        redirectTo: '/phones'
+      });
+  }]);
